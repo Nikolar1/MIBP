7c08bcb6769b0341455c740c44da5c6d6eac54e6
Nikolar1 <nikolajazak@gmail.com>
2018-04-27 09:48:49 +0200

    Dodao prepacivanje preko js

---
+++ MIBP Sajt Bootstrap/index.js
@@ -0,0 +1,24 @@
+document.getElementById("Vidakovic").onclick = function(event) {
+    window.location.href = "./Nastavnik1/index.html";
+}
+function Vidakovic() {
+    window.location.href = "./Nastavnik1/index.html";
+}
+function Vidakovicnaz() {
+    window.location.href = "../Nastavnik1/index.html";
+}
+function Popov() {
+    window.location.href = "./Nastavnik2/index.html";
+}
+function Popovnaz() {
+    window.location.href = "../Nastavnik2/index.html";
+}
+function Kupusinac() {
+    window.location.href = "./Nastavnik3/index.html";
+}
+function Kupusinacnaz() {
+    window.location.href = "../Nastavnik3/index.html";
+}
+function Pocetna() {
+    window.location.href = "../index.html";
+}
